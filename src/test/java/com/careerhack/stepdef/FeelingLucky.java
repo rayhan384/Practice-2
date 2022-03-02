63284f7cf7971e5617902b22831b0d26efa52991
Rayhan <rayhan384>
2022-03-02 02:02:55 -0500

    My first commit

---
+++ src/test/java/com/careerhack/stepdef/FeelingLucky.java
@@ -0,0 +1,38 @@
+package com.careerhack.stepdef;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class FeelingLucky {
+
+	
+	@Given("I am on the google homepage")
+	public void i_am_on_the_google_homepage() {
+	    
+	}
+
+	@When("I enter search {string}")
+	public void i_enter_search(String string) {
+	    
+	}
+
+	@When("I click on I am feeling lucky button")
+	public void i_click_on_i_am_feeling_lucky_button() {
+	    
+	}
+
+	@Then("I land on the first result homepage")
+	public void i_land_on_the_first_result_homepage() {
+	   
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
